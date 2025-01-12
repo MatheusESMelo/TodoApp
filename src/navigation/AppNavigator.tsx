a965fbd95d3eaf220aeff2950fa930d3505b7904
MatheusESMelo <matheus.estevesmelo@gmail.com>
2025-01-11 21:15:42 -0300

    Adding the app navigator whit list and completed tasks screen.

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-// import TodoListScreen from '../screens/TodoListScreen'; TODO Future screens
-// import CompletedTasksScreen from '../screens/CompletedTasksScreen'; TODO Future screens
-import TestScreen from "../screens/TestScreen";
+
+import TodoListScreen from "../screens/TodoListScreen";
+import CompletedTasksScreen from "../screens/CompletedTasksScreen";
 
 const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => {
+const AppNavigator: React.FC = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        {/* <Tab.Screen name="Todo List" component={TodoListScreen} /> TODO Future screens */}
-        {/* <Tab.Screen name="Completed Tasks" component={CompletedTasksScreen} /> TODO Future screens */}
-        <Tab.Screen name="TestScreen" component={TestScreen} />
+        <Tab.Screen name="Todo List" component={TodoListScreen} />
+        <Tab.Screen name="Completed Tasks" component={CompletedTasksScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
